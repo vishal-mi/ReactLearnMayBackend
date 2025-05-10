90e76c23eca511e8843460c858ab23745f7f99b5
vishal.mishra <vishal.mishra@tavant.com>
2025-05-10 19:25:33 +0530

    commit 2

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} with address  http://localhost:3001`)
 })
